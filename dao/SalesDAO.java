88e3a782b02ea7163951364420b687ed80f4dd04
GalSimini <gbielsimini@gmail.com>
2024-05-20 10:24:56 -0300

    incrementação dos updates no SupplierDAO

--- dao/SalesDAO.java
+++ dao/SalesDAO.java
@@ -1,4 +1,20 @@
 package dao;
 
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import model.Products;
+
 public class SalesDAO {
+
+    private Conexao conexao;
+    private String query;
+    private PreparedStatement ps;
+    private ResultSet rs;
+
+    public SalesDAO(){conexao = Conexao.getConexao();}
+
+
+
+
 }
