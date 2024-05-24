3a29a87cfe4f812bf1786a025bf1b353faf9ec9b
fapinter <fpinterich@hotmail.com>
2024-05-23 22:16:23 -0300

    login made in Main, abstraction started

--- Main.java
+++ Main.java
@@ -3,38 +3,38 @@ import model.*;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.PreparedStatement;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String args[]) {
-        PersonDAO pDAO = new PersonDAO();
-        SupplierDAO sDAO = new SupplierDAO();
+        PersonDAO peDAO = new PersonDAO();
+        SupplierDAO suDAO = new SupplierDAO();
+        ProductsDAO poDAO = new ProductsDAO();
+        SalesDAO saDAO = new SalesDAO();
 
-        // Exemplo de inserção de um fornecedor
-        // Supplier sp = new Supplier("03254085000122", "ProdutoraMT", "Tarso Bertolini",
-        // "tarsinho@outlook.com", "123456", "41992730411", "Curitiba",
-        // "Paraná", "Brasil", "Rua Manoel Eufrásio", 480);
-
-        // Scanner sc = new Scanner(System.in);
-        // System.out.println("Digite o email para remover");
-        // String email = sc.nextLine();
-
-        ResultSet rs = sDAO.listSupplier();
-        try {
-            int i = 1;
-            while (rs.next()) {
-                System.out.println("*** Supplier " + i + " ***");
-                System.out.println("Number_address : " + rs.getString("number_address"));
-                i += 1;
+        Scanner sc = new Scanner(System.in);
+        boolean login = false;
+        while (!login){
+            System.out.print("LOGIN: ");
+            String email = sc.nextLine();
+            System.out.print("SENHA: ");
+            String senha = sc.nextLine();
+            int menu = peDAO.login(email,senha);
+            switch (menu){
+                case 1:
+                    System.out.println("Bem vindo Admin");
+                    MenuPrincipal.menuAdmin();
+                    login = true;
+                case 2:
+                    System.out.println("Bem vindo Vendedor");
+                    MenuPrincipal.menuSalesman();
+                    login = true;
+                case 0:
+                    System.out.println("Login ou senha incorretos");
             }
-        } catch (SQLException ex) {
-            ex.printStackTrace();
         }
-
-        System.out.println("rodou");
-
-        MenuPrincipal.menu();
     }
 }
 
@@ -42,7 +42,8 @@ class MenuPrincipal {
 
     private static Scanner scanner = new Scanner(System.in);
 
-    public static void menu() {
+    public static void menuSalesman(){}
+    public static void menuAdmin() {
         int opcaoMenuPrincipal;
 
         do {
@@ -58,16 +59,29 @@ class MenuPrincipal {
 
             switch (opcaoMenuPrincipal) {
                 case 1:
-                    menuPessoa();
-                    break;
+                    int opcaoMenuSecundario = secondaryMenu("Pessoa");
+                    switch(opcaoMenuSecundario){
+                        case 1:
+                            System.out.println("Inserir");
+                        case 2:
+                            System.out.println("Remove");
+                        case 3:
+                            System.out.println("List");
+                        case 4:
+                            System.out.println("Edit");
+                        case 0:
+                            System.out.println("Go back");
+                        default:
+                            System.out.println("Not found, return");
+                    }
                 case 2:
-                    menuProduto();
+                    secondaryMenu("Produto");
                     break;
                 case 3:
                     menuVenda();
                     break;
                 case 4:
-                    menuFornecedor();
+                    secondaryMenu("Fornecedor");
                     break;
                 case 0:
                     System.out.println("Saindo do programa...");
@@ -77,7 +91,7 @@ class MenuPrincipal {
             }
         } while (opcaoMenuPrincipal != 0);
 
-        scanner.close();
+
     }
 
     private static void menuPessoa() {
@@ -214,6 +228,19 @@ class MenuPrincipal {
                     System.out.println("Opção inválida. Tente novamente.");
             }
         } while (opcaoMenuFornecedor != 0);
+    }
+    public static int secondaryMenu(String type_menu){
+        Scanner sc = new Scanner(System.in);
+        int optionMenu;
+        System.out.println("Menu "+type_menu);
+        System.out.println("1. Inserir "+type_menu+": ");
+        System.out.println("2. Atualizar "+type_menu+": ");
+        System.out.println("3. Deletar "+type_menu+": ");
+        System.out.println("4. Listar "+type_menu+": ");
+        System.out.println("0. Voltar ao Menu Principal: ");
+        System.out.print("Digite a opção desejada: ");
+        optionMenu = sc.nextInt();
+        return optionMenu;
     }
 }
 
